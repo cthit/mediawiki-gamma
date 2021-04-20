3257abeec8a9b5ae5580b0eda0bc775bcaff151c
Rasmus Rosengren <rasmus.rosengren@pm.me>
2021-04-20 15:57:45 +0200

    path from environment

--- LocalSettings.php
+++ LocalSettings.php
@@ -27,7 +27,7 @@ $wgMetaNamespace = $_ENV["WIKI_NAME"];
 ## For more information on customizing the URLs
 ## (like /w/index.php/Page_title to /wiki/Page_title) please see:
 ## https://www.mediawiki.org/wiki/Manual:Short_URL
-$wgScriptPath = "";
+$wgScriptPath = $_ENV["PATH"];
 
 ## The protocol and server name to use in fully-qualified URLs
 $wgServer = $_ENV["ROOT_URL"];
